c46a74210b5bfedad83b81ab733e0f08c38f5271
Brecht Machiels <brecht@mos6581.org>
2022-01-08 12:56:22 +0100

    Organize backup/snapshots directories differently
    ... to better suit backing up multiple paths
    
    This also fixes the purging.

--- rclone_backup.py
+++ rclone_backup.py
@@ -63,8 +63,12 @@ class RcloneBackup:
     self.echo = echo
 
   @property
+  def name(self):
+    return self.destination.name
+
+  @property
   def logs_path(self):
-    return PATH / "logs" / self.destination.name
+    return PATH / "logs" / self.name
 
   def rclone(self, *args, dry_run=None, input=None, capture=False):
     dry_run = self.dry_run if dry_run is None else dry_run
@@ -102,13 +106,13 @@ class RcloneBackup:
 
     timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M")
     last_log, last_timestamp = self.get_last_log()
-    log_filename = f"{self.destination.name}_{timestamp}_{cmd}.log"
+    log_filename = f"{self.name}_{timestamp}_{cmd}.log"
     log_path = self.logs_path / log_filename
-    backupdir = f"{self.destination}_{last_timestamp}"
+    backupdir = self.destination / last_timestamp
     try:
       self.rclone(cmd, "--log-file", log_path, "--backup-dir", backupdir,
                   "--exclude-from", self.exclude_file, *args, *self.BACKUP_ARGS,
-                  *self.extra_args, self.source, self.destination)
+                  *self.extra_args, self.source, self.destination / "latest")
     except subprocess.CalledProcessError as cpe:
       rc = cpe.returncode
       info = RCLONE_EXIT_CODES[rc]
@@ -129,16 +133,14 @@ class RcloneBackup:
       raise SystemExit(rc)
     finally:
       self.record_backup_size(backupdir)
-      self.rclone("move", f"{self.destination.parent}/{last_log}", backupdir)
-      self.rclone("copy", log_path, self.destination.parent)
+      self.rclone("move", self.destination / last_log, backupdir)
+      self.rclone("copy", log_path, self.destination)
     # self.purge(destination)
 
   def combine(self, old, new):
-    _, old_dir = old.rsplit("/", maxsplit=1)
-    _, new_dir = new.rsplit("/", maxsplit=1)
-    if echo:
-      print(f"# combining {old_dir} and {new_dir} backups")
-    log_path = self.logs_path / f"{old_dir}_combine.log"
+    if self.echo:
+      print(f"# combining {old.name} and {new.name} backups")
+    log_path = self.logs_path / f"{self.name}_{old.name}_combine.log"
     self.rclone("move", new, old, "--delete-empty-src-dirs", "--log-file",
                 log_path, "--log-level", "INFO")
     self.rclone("rmdir", new)
@@ -148,16 +150,15 @@ class RcloneBackup:
 
   def purge(self):
     now = datetime.now()
-    parent_dir, backup_dir = self.destination.parent, self.destination.name
-    backups = self.list_files(parent_dir, f"{backup_dir}_*/", dirs_only=True)
-    offset = len(backup_dir) + 1
+    backups = self.list_files(self.destination, exclude="/latest/",
+                              recursive=False, dirs_only=True)
     daily = None
     weekly = None
     combine_path = None
     combining = False
     for backup in backups:
-      path = f"{parent_dir}/{backup}"
-      timestamp = datetime.fromisoformat(backup[offset:])
+      path = self.destination / backup
+      timestamp = datetime.fromisoformat(backup)
       age = now - timestamp
       day = timestamp.date()
       month = day.year, day.month
@@ -168,59 +169,59 @@ class RcloneBackup:
       elif age < timedelta(days=31):    # keep daily backups
         if daily and daily == day:
           combining = True
-          combine_path = combine(combine_path, path, *extra_args, dry_run=dry_run,
-                                 echo=echo)
+          combine_path = self.combine(combine_path, path)
           continue
         daily = day
       else:                             # keep weekly backups
         if weekly and weekly == week:
           combining = True
-          combine_path = combine(combine_path, path, *extra_args, dry_run=dry_run,
-                                 echo=echo)
+          combine_path = self.combine(combine_path, path)
           continue
         weekly = week
 
       if combining:
-        if echo:
+        if self.echo:
           print(f'# calculate size for {combine_path}')
-        _, combine_dir = combine_path.rsplit("/", maxsplit=1)
-        self.rclone("delete", "--include", f"/{combine_dir}_size_*",
+        self.rclone("delete", "--include", f"/{self.name}_*_size_*",
                     combine_path)
         self.record_backup_size(combine_path)
         combining = False
 
-      if echo:
+      if self.echo:
         print('# keeping ', timestamp)
       combine_path = path
 
 
-  def list_files(self, path, pattern, dirs_only=False, files_only=False):
+  def list_files(self, path, include=None, exclude=None, recursive=True,
+                 dirs_only=False, files_only=False):
     args = ((["--dirs-only"] if dirs_only else [])
-            + (["--files-only"] if files_only else []))
-    list_cmd = self.rclone("lsf", "--recursive", "--include", pattern, path,
-                           *args, dry_run=False, capture=True)
+            + (["--files-only"] if files_only else [])
+            + (["--include", include] if include else [])
+            + (["--exclude", exclude] if exclude else [])
+            + (["--recursive"] if recursive else []))
+    list_cmd = self.rclone("lsf", path, "--dir-slash=false", *args,
+                           dry_run=False, capture=True)
     return sorted(list_cmd.stdout.split())
 
   def get_last_log(self):
-    parent_dir, backup_dir = self.destination.parent, self.destination.name
-    logs = self.list_files(parent_dir, f"/{backup_dir}_*.log", files_only=True)
+    logs = self.list_files(self.destination, include=f"/{self.name}_*.log",
+                           files_only=True)
     if not logs:  # this is the first backup
       return None, None
     try:
       last_log, = logs
     except ValueError:
       raise SystemExit(f"There should only be a single '{backup_dir}' log file"
-                       f" in {parent_dir}!")
+                       f" in {self.destination}!")
     _, last_backup_timestamp, _ = last_log.split("_")
     return last_log, last_backup_timestamp
 
   def get_last_backup_age(self):
     now = datetime.now()
-    parent_dir, backup_dir = self.destination.parent, self.destination.name
     last_log, last_timestamp = self.get_last_log()
     last_age = now - datetime.fromisoformat(last_timestamp)
-    sync_logs = self.list_files(parent_dir,
-                                f"/{backup_dir}_*/{backup_dir}_*_sync.log",
+    sync_logs = self.list_files(self.destination,
+                                include=f"/*/{self.name}_*_sync.log",
                                 files_only=True)
     if sync_logs:
       _, last_sync_timestamp, _ = sorted(sync_logs)[-1].rsplit("_", maxsplit=2)
@@ -230,26 +231,25 @@ class RcloneBackup:
                 else now - datetime.fromisoformat(last_sync_timestamp))
     return last_age, sync_age
 
-  def record_backup_size(self, backupdir):
-    backupdir = Path(backupdir)
-    backup_name = backupdir.name
+  def record_backup_size(self, snapshot):
     size_cmd = self.rclone("size", "--json", "--exclude",
-                           f"/{backup_name}_*.log", backupdir, capture=True)
+                           f"/{self.name}_*.log", snapshot, capture=True)
     size = 0 if self.dry_run else json.loads(size_cmd.stdout)["bytes"]
-    size_filename = backup_name + f"_size_{size}"
-    self.rclone("touch", backupdir / size_filename)
+    size_filename = f"{self.name}_{snapshot.name}_size_{size}"
+    self.rclone("touch", snapshot / size_filename)
 
-  RE_SIZE = re.compile(r"^(.+)\/\1_size_(\d+)$")
+  RE_SIZE = re.compile(r"^(.+)\/.*_\1_size_(\d+)$")
 
   def snapshot_sizes(self):
     parent_dir, name = self.destination.parent, self.destination.name
-    size_files = self.list_files(parent_dir, f"/{name}_*/{name}_*_size_*",
+    size_files = self.list_files(self.destination,
+                                 include=f"/*/{self.name}_*_size_*",
                                  files_only=True)
     total = 0
     for path in size_files:
-      name, size_str = self.RE_SIZE.match(path).groups()
+      timestamp, size_str = self.RE_SIZE.match(path).groups()
       size = int(size_str)
-      print(f"{name:42} {human_size(size):>12}")
+      print(f"{timestamp:42} {human_size(size):>12}")
       total += size
     print(f"Total: {human_size(total):>12}")
 
