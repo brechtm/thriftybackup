6d67d9adf4d1e5c6d0087aee979bdaec1c09b8ac
Brecht Machiels <brecht@mos6581.org>
2023-03-13 23:15:22 +0100

    Move over more code from rclone_backup.py
    - determine time difference between last backup and local snapshot
      - only backup if difference > threshold from configuration file
    - set a backup dir for incremental backups
      - copy logs from previous backup to backup dir
      - record size of backup dir in name of empty file
    - Configuration: parse all of backups.toml and store it
    - add list subcommand: list remote snapshot sized

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import re
 import sys
 import tomllib
 
-from datetime import timedelta
+from datetime import datetime, timedelta
+from itertools import chain
 from pathlib import Path
-from subprocess import run, Popen, PIPE
+from subprocess import run, Popen, PIPE, CalledProcessError
 from tempfile import TemporaryDirectory
 from threading import Thread
 
@@ -79,6 +80,7 @@ class Directory(Entry):
 
 
 PREFIXES = {40: 'T', 30: 'G', 20: 'M', 10: 'K', 0: ' '}
+EXPONENTS = {value: key for key, value in PREFIXES.items()}
 
 
 def format_size(n_bytes, align=False):
@@ -98,12 +100,12 @@ def find_large_entries(entry, threshold):
     except AttributeError:  # entry is a file
         yield [], entry
     else:                   # entry is a directory
-        bleh = True
-        for name, e in entries.items():
-            for parts, entry in find_large_entries(e, threshold):
+        yield_this_dir = True
+        for name, child in entries.items():
+            for parts, entry in find_large_entries(child, threshold):
                 yield [name, *parts], entry
-                bleh = False
-        if bleh:
+                yield_this_dir = False
+        if yield_this_dir:
             yield [''], entry
 
 
@@ -143,7 +145,8 @@ RE_SNAPSHOT = re.compile('com\.apple\.TimeMachine\.(\d{4}-\d{2}-\d{2}-\d{6})\.lo
 
 class RCloneBackup:
 
-    def __init__(self, name, source, destination, interval, threshold):
+    def __init__(self, name, source, destination, interval, threshold,
+                 echo=False, dry_run=False):
         self.name = name
         self.source = Path(source)
         self.destination = Path(destination)
@@ -151,13 +154,15 @@ class RCloneBackup:
         self.threshold = threshold
         self.pid = None
         self.interface = None
+        self.tree = None
         
-        self._tempdir = TemporaryDirectory()
-        self.mount_point = Path(self._tempdir.name)
-        self.timestamp = self.mount_last_snapshot()        
-        self.snapshot_source = self.mount_point / self.source
-        self.destination_latest = self.destination / 'latest'        
-        self.thread = None
+        self.echo = True        # TODO: replace when things are working
+        self.dry_run = True     # TODO: replace when things are working
+
+        self.device, self.snapshot = self.get_last_snapshot()
+        self.timestamp = RE_SNAPSHOT.match(self.snapshot).group(1)
+        self._tempdir = None
+        self.mount_point = None
 
     @property
     def exclude_file(self):
@@ -175,35 +180,102 @@ class RCloneBackup:
     def ncdu_export_path(self):
         return self.file_path('ncdu', 'json')
 
-    def __del__(self):
+    def cleanup(self):
         # TODO: stop thread
-        self.unmount_snapshot()
-        
-    def backup(self):
-        with pid.PidFile(piddir=PATH) as self.pid:
-            self.interface = AppInterface(self)
-            self.thread = Thread(target=self.backup_thread, daemon=True)
-            self.thread.start()
-            self.interface.start_app()
-        
-    def mount_last_snapshot(self):
-        # TODO: manually create new snapshot?
+        if hasattr(self, '_tempdir'):
+            self.unmount_snapshot()
+            del self._tempdir
+        self.pid.close()
+
+    def get_last_snapshot(self):
         du_info = run(['diskutil', 'info', '-plist', 'Data'],
                       check=True, capture_output=True).stdout
         device = plistlib.loads(du_info)['DeviceIdentifier']
         snapshots = run(['diskutil', 'apfs', 'listSnapshots', '-plist', device],
                         check=True, capture_output=True).stdout
-        last_snapshot = plistlib.loads(snapshots)['Snapshots'][-1]['SnapshotName']
-        snapshot_timestamp = RE_SNAPSHOT.match(last_snapshot).group(1)
-        print('Mounting', last_snapshot, 'at', self.mount_point)
-        run(['mount_apfs', '-s', last_snapshot, f'/dev/{device}', self.mount_point],
-             check=True)
-        return snapshot_timestamp
+        snapshot = plistlib.loads(snapshots)['Snapshots'][-1]['SnapshotName']
+        return device, snapshot
+
+    def mount_snapshot(self):
+        self._tempdir = TemporaryDirectory()
+        self.mount_point = Path(self._tempdir.name)
+        print(f'Mounting {self.snapshot} at {self.mount_point}')
+        run(['mount_apfs', '-s', self.snapshot,
+             f'/dev/{self.device}', self.mount_point], check=True)
         
     def unmount_snapshot(self):
         run(['umount', self.mount_point], check=True)        
 
-    def backup_thread(self):
+    def backup(self, force=False):
+        with pid.PidFile(piddir=PATH) as self.pid:
+            return self._backup(force)
+
+    def _backup(self, force):
+        local_timestamp = datetime.fromisoformat(self.timestamp)
+        try:
+            last_log = self.get_last_log()
+        except CalledProcessError as error:
+            if error.returncode == 1:   # connection error
+                return False
+            raise
+        
+        if last_log:
+            last_age = (local_timestamp
+                            - datetime.fromisoformat(timestamp_from_log(last_log)))
+            forced = force and last_age > timedelta(0)   # there must be changes
+            if last_age < self.interval and not forced:
+                print(f"Last backup is only {last_age} old (< {self.interval})")
+                return False
+        self.mount_snapshot()
+        self.interface = AppInterface(self)
+        self.thread = Thread(target=self.backup_thread, args=[last_log],
+                             daemon=True)
+        self.thread.start()
+        self.interface.start_app()
+
+    def rclone(self, *args, dry_run=None, capture=False) -> Popen or None:
+        """Run rclone with the given arguments
+        
+        Args:
+          args: command line arguments passed to rclone
+          dry_run: if not None, overrides dry_run set for the instance
+          capture: capture the output (in stdout attribute of return value)
+        
+        Returns:
+          Popen
+        """
+        dry_run = self.dry_run if dry_run is None else dry_run
+        cmd = ['rclone', *args]
+        if self.echo or dry_run:
+            print(' '.join(map(str, cmd)))
+        if not dry_run:
+            return run(cmd, cwd=PATH, capture_output=capture, encoding="utf-8",
+                       check=True)
+
+    def list_files(self, path, include=None, exclude=None, recursive=True,
+                   dirs_only=False, files_only=False):
+        args = ((['--dirs-only'] if dirs_only else [])
+                + (['--files-only'] if files_only else [])
+                + (['--include', include] if include else [])
+                + (['--exclude', exclude] if exclude else [])
+                + (['--recursive'] if recursive else []))
+        list_cmd = self.rclone('lsf', path, '--dir-slash=false', *args,
+                               dry_run=False, capture=True)
+        return sorted(list_cmd.stdout.split())
+
+    def get_last_log(self):
+        logs = self.list_files(self.destination, include=f"/{self.name}_*.log",
+                               recursive=False, files_only=True)
+        if not logs:  # this is the first backup
+            return None, None
+        try:
+            last_log, = logs
+        except ValueError:
+            raise SystemExit(f"There should only be a single sync log file in"
+                             f" {self.destination}!")
+        return last_log
+
+    def backup_thread(self, last_log):
         self.tree, large_entries = self.backup_scout()
         backup_size = self.tree.size
         if large_entries:
@@ -216,54 +288,106 @@ class RCloneBackup:
         else:
             exclude = []
         self.interface.startBackup_(backup_size)
-        self.backup_sync(exclude)
-        self.unmount_snapshot()
+        backup_dir = (self.destination / timestamp_from_log(last_log)
+                      if last_log else None)
+        sync_log = self.backup_sync(backup_dir, exclude)
+        if backup_dir:
+            last_logs = '/' + last_log.replace('sync.log', '*.*')
+            self.rclone('move', self.destination, '--include', last_logs,
+                        backup_dir)
+            local_logs = self.file_path('*', '*')
+            self.rclone('copy', local_logs.parent, '--include',
+                        local_logs.name, self.destination)
+            self.record_backup_size(backup_dir)
+        self.cleanup()
         self.interface.quitApp()
 
-    def sync_popen(self, *args):
+    def sync_popen(self, *args, dry_run=False):
+        snapshot_source = self.mount_point / self.source.relative_to('/')
+        destination = self.destination / 'latest'
+        extra = list(chain(['--dry-run'] if dry_run else [],
+                           ['--progress'] if self.echo else []))
         cmd = ['rclone', 'sync', '--use-json-log', '--fast-list', '--links',
                '--track-renames', '--track-renames-strategy', 'modtime,leaf',
-               *args,
-               self.snapshot_source, self.destination_latest]
+               '--retries', '1', *args, *extra, snapshot_source, destination]
+        if self.echo:
+            print(' '.join(map(str, cmd)))
         return Popen(cmd, stderr=PIPE)
 
     def backup_scout(self):
         tree = Directory('')
-        rclone_sync = self.sync_popen('--dry-run', '--progress',
-                                      '--exclude-from', self.exclude_file)
-        scout_log = self.file_path('scout', 'log')
-        with scout_log.open('wb') as log:
-            for line in rclone_sync.stderr:
-                log.write(line)
-                msg = json.loads(line)
-                if msg.get('skipped') == 'copy':
-                    rel_path = Path(msg['object'])
-                    tree.add_file(rel_path, msg['size'])
+        exclude = (['--exclude-from', self.exclude_file]
+                   if self.exclude_file.exists() else [])
+        try:
+            rclone_sync = self.sync_popen(*exclude, dry_run=True)
+            scout_log = self.file_path('scout', 'log')
+            with scout_log.open('wb') as log:
+                for line in rclone_sync.stderr:
+                    log.write(line)
+                    msg = json.loads(line)
+                    if msg.get('skipped') == 'copy':
+                        rel_path = Path(msg['object'])
+                        tree.add_file(rel_path, msg['size'])
+        except CalledProcessError as cpe:
+            # TODO: interpret rclone_sync.returncode
+            raise
         tree.calculate_size()
         self.tree = tree
         write_ncdu_export(self.source, tree, self.ncdu_export_path)
         return tree, sorted(find_large_entries(tree, self.threshold),
                             key=lambda item: item[1].size, reverse=True)
 
-    def backup_sync(self, exclude):
         # TODO: caffeinate
         # FIXME: abort subprocesses on App quit
+    def backup_sync(self, backup_dir, exclude):
         files_txt = self.file_path('files', 'txt')
         with files_txt.open('w') as files:
             for file in self.tree.iter_files(exclude=exclude):
                 print(file.path, file=files)
         files_txt = self.file_path('files', 'txt')
-        rclone_sync = self.sync_popen('--dry-run', '--progress',
-                                      '--files-from-raw', files_txt)
-        transferred = 0
-        sync_log = self.file_path('sync', 'log')
-        with sync_log.open('wb') as log:
-            for line in rclone_sync.stderr:
-                log.write(line)
-                msg = json.loads(line)
-                if size := msg.get('size'):
-                    transferred += size
-                    self.interface.updateProgress_(transferred)
+        backupdir_args = ['--backup-dir', backup_dir] if backup_dir else []
+        try:
+            sync = self.sync_popen('--files-from-raw', files_txt,
+                                   *backupdir_args, dry_run=self.dry_run)
+            transferred = 0
+            sync_log = self.file_path('sync', 'log')
+            with sync_log.open('wb') as log:
+                for line in sync.stderr:
+                    log.write(line)
+                    msg = json.loads(line)
+                    if size := msg.get('size'):
+                        transferred += size
+                        self.interface.updateProgress_(transferred)
+        except CalledProcessError as cpe:
+            raise
+        return sync_log
+
+    def record_backup_size(self, backupdir):
+        size_cmd = self.rclone('size', '--json', '--exclude',
+                               f'/{self.name}_*.log', backupdir, capture=True)
+        size = 0 if self.dry_run else json.loads(size_cmd.stdout)['bytes']
+        size_filename = f'{self.name}_{backupdir.name}_size_{size}'
+        self.rclone('touch', backupdir / size_filename)
+
+    RE_SIZE = re.compile(r"^(.+)\/.*_\1_size_(\d+)$")
+
+    def print_snapshot_sizes(self):
+        size_files = self.list_files(self.destination,
+                                    include=f"/*/{self.name}_*_size_*",
+                                    files_only=True)
+        total = 0
+        print(f"Size of snapshots in {self.destination}")
+        for path in size_files:
+            timestamp, size_str = self.RE_SIZE.match(path).groups()
+            size = int(size_str)
+            print(f"{timestamp:18} {format_size(size, True):>12}")
+            total += size
+        print(f"{'Total:':18} {format_size(total, True):>12}")
+
+
+def timestamp_from_log(log_filename):
+    _, timestamp, _ = log_filename.rsplit('_', maxsplit=2)
+    return timestamp
 
 
 from AppKit import NSAttributedString
@@ -327,6 +451,9 @@ class AppInterface(NSObject):
 
     def _abortBackup_(self, _):
         self.process
+        
+    def cleanUp(self):
+        self.process.cleanup()
 
 
 class MenuBarApp(rumps.App):
@@ -345,7 +472,7 @@ class MenuBarApp(rumps.App):
         self.add_menuitem('Show Files', self.show_files, 'f')
 
     def prepare(self):
-        self.add_menuitem('Preparing backup...')
+        self.add_menuitem('Determining backup size...')
 
     def set_title(self, title, color=None):
         self.title = f' {title}'
@@ -419,7 +546,7 @@ class MenuBarApp(rumps.App):
         self.quit()
         
     def quit(self):
-        self.interface.process.pid.close()
+        self.interface.cleanUp()
         rumps.quit_application()
 
 
@@ -437,9 +564,63 @@ tell app "Terminal"
 end tell
 """
 
+RE_KEEP = re.compile(r'(?P<days>\d+)\s*(d(ays?)?)?', re.IGNORECASE)
 
 RE_INTERVAL = re.compile(r'((?P<days>\d+?)\s*(d|days?))?\s*'
-                         r'((?P<hours>\d+?)\s*(h|hours?))?')
+                         r'((?P<hours>\d+?)\s*(h|hours?))?', re.IGNORECASE)
+
+RE_THRESHOLD = re.compile(r'(?P<number>\d+)\s*(?P<unit>[KMGT])B?',
+                          re.IGNORECASE)
+
+
+class Configuration(dict):
+    def __init__(self, config_path, echo, dry_run) -> None:
+        self.config_path = config_path
+        self.echo = echo
+        self.dry_run = dry_run
+        with config_path.open('rb') as f:
+            self.toml = tomllib.load(f)
+        self.rclone = self.toml.get('rclone', 'rclone')
+        self.bandwidth_limit = self.toml.get('bandwidth_limit', None)
+        self.keep_all = self._parse_keep('keep_all', '7 days')
+        self.keep_daily = self._parse_keep('keep_daily', '31 days')
+        for key, value in self.toml.items():
+            if isinstance(value, dict):
+                self[key] = self._create_backup(key, value)
+
+    def _syntax_error(self, attribute, section=None):
+        sect = f' in section [{section}]' if section else ''
+        raise SystemExit(f"{self.config_path.name}{sect}: value for"
+                         f" '{attribute}' could not be parsed")
+
+    def _parse_keep(self, attribute, default):
+        value = self.toml.get('keep_all', default)
+        keep_match = RE_KEEP.fullmatch(value.strip())
+        try:
+            return int(keep_match.group('days'))
+        except AttributeError:
+            self._syntax_error(attribute)
+
+    def _create_backup(self, name, cfg):
+        src, dest = cfg['source'], cfg['destination']
+        interval_match = RE_INTERVAL.fullmatch(cfg['interval'].strip())
+        try:
+            interval = timedelta(**{key: int(value) for key, value in
+                                    interval_match.groupdict(0).items()})
+        except AttributeError:
+            self._syntax_error('interval', section=name)
+        threshold = cfg.get('threshold')
+        if threshold:
+            try:
+                threshold_match = RE_THRESHOLD.fullmatch(threshold.strip())
+                exp = EXPONENTS[threshold_match.group('unit')]
+                threshold = int(threshold_match.group('number')) * 2**exp
+            except TypeError:
+                pass    # threshold is a number
+            except AttributeError:
+                self._syntax_error('threshold', section=name)
+        return RCloneBackup(name, src, dest, interval, threshold,
+                            echo=self.echo, dry_run=self.dry_run)
 
 
 if __name__ == "__main__":
@@ -450,35 +631,33 @@ if __name__ == "__main__":
                         help="Dry-run rclone commands")
     subparsers = parser.add_subparsers(dest='command', )#help='')
     parser_backup = subparsers.add_parser('backup', help='start a backup')
-    parser_backup.add_argument('--force', action='store_true',
+    parser_backup.add_argument('-f', '--force', action='store_true',
                                help="Force a backup regardless of when the last"
                                     " backup was performed")
     parser_list = subparsers.add_parser('list', help='list backup snapshots')
-    parser_list.add_argument('backup', 
-                             help="The backup configuration for which to list"
-                                  " snapshots")
+    parser_list.add_argument('backup', help="The backup configuration for which"
+                                            " to list snapshots")
     args = parser.parse_args()
     
-    with (PATH / 'backups.toml').open('rb') as f:
-       config = tomllib.load(f)
-    backups = {key: value for key, value in config.items()
-               if isinstance(value, dict)}
+    config = Configuration(PATH / 'backups.toml',
+                           echo=args.echo, dry_run=args.dry_run)
 
     match args.command:
         case 'backup':
-            for name, cfg in backups.items():
-                interval_match = RE_INTERVAL.fullmatch(cfg['interval'].strip())
-                interval = timedelta(**{key: int(value) for key, value in
-                                        interval_match.groupdict(0).items()})
+            for backup in config.values():
                 try:
-                    backup = RCloneBackup(name, cfg['source'], cfg['destination'],
-                                          interval, cfg['threshold'])
-                    backup.backup()
+                    if backup.backup(force=args.force):
+                        break   # only continue to next backup if current one is skipped
                 except pid.PidFileError:
                     if sys.stdout.isatty():
                         raise SystemExit("An rclone backup is already in progress")
                 break
         case 'list':
-            raise NotImplementedError
+            if args.backup not in config:
+                msg = (f"There is no backup named '{args.backup}'. Choose one"
+                       f" from:\n" + '\n'.join(f'- {n}' for n in config))
+                raise SystemExit(msg)
+            backup = config[args.backup]
+            backup.print_snapshot_sizes()
         case _:
-            raise NotImplementedError
+            parser.print_help()
